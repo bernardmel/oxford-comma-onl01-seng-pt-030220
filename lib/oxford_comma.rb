7e77450b668d45a7059916a38eeaffae4d3fedde
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-09 23:41:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,8 +8,8 @@ elsif array.size ==2
 elsif array.size ==3
   return array[0..1].join(", ") + ", and " + array[2]
 
-else array.size ==4
-  return array[0..3].join(", ") + ", and " + array[4]
+else array.size ==6
+  return array[0..5].join(", ") + ", and " + array[6]
 
 end
 end
