db70e1ab05a84ed03a1aa37f791a6599e38eb0c9
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-10 00:02:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,7 @@ elsif array.size ==3
   return array[0..1].join(", ") + ", and " + array[2]
 
 else
-  return array[0..-2].join(", ") + ", and " + array[-1]
+  return array[0..2].join(", ") + ", and " + array[1]
 
 end
 end
